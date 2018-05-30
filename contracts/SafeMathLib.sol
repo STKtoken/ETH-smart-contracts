ec84d8a9a4e70cfb5abc3eec07c912987468a4a3
Natalie Chin <chinnh@mcmaster.ca>
2018-05-30 13:51:22 -0400

    Modified and deleted files related to STKToken

--- contracts/SafeMathLib.sol
+++ contracts/SafeMathLib.sol
@@ -12,23 +12,23 @@ pragma solidity ^0.4.23;
  */
 library SafeMathLib {
 
-  function times(uint a, uint b)
-     internal pure returns (uint) {
+    function times(uint a, uint b)
+        internal pure returns (uint) {
         uint c = a * b;
         assert(a == 0 || c / a == b);
         return c;
-  }
+    }
 
-  function minus(uint a, uint b)
-      internal pure returns (uint) {
-          assert(b <= a);
-          return a - b;
-  }
+    function minus(uint a, uint b)
+        internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  function plus(uint a, uint b)
-      internal pure returns (uint) {
-      uint c = a + b;
-      assert(c>=a);
-      return c;
-  }
+    function plus(uint a, uint b)
+        internal pure returns (uint) {
+        uint c = a + b;
+        assert(c>=a);
+        return c;
+    }
 }
